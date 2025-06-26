259ccd6b60a270d50ad04b8bfaa6cf8d16e0423c
Will VanderWyden <vanderwyden@gmail.com>
2025-06-26 14:17:30 +0900

    adding path 3

--- allLocations.js
+++ allLocations.js
@@ -8,3 +8,7 @@ export const fortSumpter = {
     options: "1. Buy Supplies\n2.Continue on the trail",
     buySupplies: "1. Buy Food\n2. Buy Wagon Wheels\n3. Leave"
 };
+
+// independence, kansas river, big blue river, fort kearney, chimney rock, fort laramie, independence rock, south pass == green river or fort bridger
+// if green river, soda springs, fort hall, snake river, fort boise, blue mountains -- fort walla walla or the dulles
+// if dulles, oregon city??
