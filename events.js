b4c1c4b131b1923336f132bbc4bfb4b7469f76ab
Will VanderWyden <vanderwyden@gmail.com>
2025-07-02 16:03:21 +0900

    randomized disease and accident

--- events.js
+++ events.js
@@ -1,9 +1,18 @@
 export const diseases = ["measles", "dysentery", "severe diarrhea", "ruptured spleen", "giant turds"];
-
 export const accidents = ["wagon axle broke", "wagon caught on fire", "attacked by bears", "volcano erupted and engulfed a wagon"];
+
 export function getRandomAccident() {
-    const index = Math.floor(Math.random() * accidents.length);
-    return accidents[index];
+    const accidentIndex = Math.floor(Math.random() * accidents.length);
+    const diseaseIndex = Math.floor(Math.random() * diseases.length);
+    const chooseEvent = (Math.floor(Math.random() * 2)) + 1;
+    
+    // decide what type of event it is
+    if (chooseEvent === 1) {
+        return accidents[accidentIndex];
+    }
+    else if (chooseEvent === 2) {
+        return diseases[diseaseIndex];
+    }
 }
 
 // export const lostDays = range(1, 10);
